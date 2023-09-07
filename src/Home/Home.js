e1b799c2024530a76ae66456cfafe5c70d4d2372
davidomotoso <davidomotoso45@gmail.com>
2023-09-07 14:54:24 +0100

    file set up

---
+++ src/Home/Home.js
@@ -0,0 +1,11 @@
+import "../index.css";
+const Home = () => {
+  return (
+    <main>
+      <h1 className="text-amber-300 bg bg-black">
+        Welcome to dodge automobile
+      </h1>
+    </main>
+  );
+};
+export default Home;
