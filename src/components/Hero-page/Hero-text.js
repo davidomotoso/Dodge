ff38a2296c27fd806b24e6a27dd0909174d6fef1
David <117993843+davidomotoso@users.noreply.github.com>
2023-09-11 10:38:04 +0100

    Done with hero carousel

--- src/components/Hero-page/Hero-text.js
+++ src/components/Hero-page/Hero-text.js
@@ -1,52 +1,30 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
 import MediaIcon from "./mediaIcon";
 
-const HeroText = ({ text }) => {
+const HeroText = ({ text, active }) => {
+  const Text = (slideIndex) => {
+    if (slideIndex === 0) {
+      return text[0].name;
+    } else if (slideIndex === 1) {
+      return text[1].name;
+    } else if (slideIndex === 2) {
+      return text[2].name;
+    } else if (slideIndex === 3) {
+      return text[3].name;
+    } else if (slideIndex === 4) {
+      return text[4].name;
+    }
+  };
   return (
     <aside className="text-white w-80 relative -mb-6 z-10 ">
-      <section className="overflow-hidden absolute -z-10 -top-20 w-[49rem]">
-        <section className="flex iems-center justify-between h-32">
-          {/* <Carousel
-            showArrows={false}
-            showThumbs={false}
-            showIndicators={false}
-            showStatus={false}
-            // autoPlay
-            infiniteLoop
-            interval={3000}
-          > */}
-          {text.map((txt, index) => (
-            <h1
-              key={index}
-              className="w-full text-[#3d3d47] font-semibold text-9xl -mb-4 "
-            >
-              {txt.name}
-            </h1>
-          ))}
-          {/* </Carousel> */}
+      <section className="overflow-hidden absolute -z-10 -top-20">
+        <section className="flex w-[49em] iems-center justify-between h-32">
+          <h1 className="w-full text-[#3d3d47] font-semibold text-9xl -mb-4 pl-1">
+            {Text(active)}
+          </h1>
         </section>
       </section>
       <section className="mb-5">
-        <section className="overflow-hidden w-[23em]">
-          <section className="w-full flex items-center justify-between">
-            {/* <Carousel
-              showArrows={false}
-              showThumbs={false}
-              showIndicators={false}
-              showStatus={false}
-              autoPlay
-              infiniteLoop
-              interval={1800}
-            > */}
-            {text.map((txt, index) => (
-              <h2 key={index} className="text-6xl font-semibold">
-                {txt.name}
-              </h2>
-            ))}
-            {/* </Carousel> */}
-          </section>
-        </section>
+        <h2 className="text-6xl font-semibold">{Text(active)}</h2>
       </section>
       <section className="mb-6 text-lg font-normal">
         <p>Expand the actual teaching of the great explorer of the truth</p>
