c521ba0b17bce916ccb2015bfe2f15d8b1600077
davidomotoso <davidomotoso45@gmail.com>
2023-09-11 10:28:48 +0100

    done with hero page carousel

--- src/components/Hero-page/Hero-image.js
+++ src/components/Hero-page/Hero-image.js
@@ -1,17 +1,18 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-const HeroImage = ({ image }) => {
+const HeroImage = ({ image, handleSlideChange }) => {
   return (
     <aside className="overflow-hidden">
-      <figure className="relative z-20 w-[54.36em] flex items-center justify-between">
+      <figure className="relative z-20 w-[54.36em] h-max flex items-center justify-between">
         <Carousel
           showArrows={false}
           showThumbs={false}
           showIndicators={false}
           showStatus={false}
+          onChange={handleSlideChange}
           autoPlay
           infiniteLoop
-          interval={1800}
+          interval={1000}
         >
           {image.map((img, index) => (
             <img key={index} src={img.src} alt={img.name} />
--- src/components/Hero-page/HeroCombo.js
+++ src/components/Hero-page/HeroCombo.js
@@ -5,6 +5,7 @@ import charger from "../../assets/Hero-images/charger.png";
 import journey from "../../assets/Hero-images/journey.png";
 import durango from "../../assets/Hero-images/durango.png";
 import dart from "../../assets/Hero-images/dart.png";
+import { useState } from "react";
 const Herocombo = () => {
   const HeroVehicles = [
     { name: "CHALLENGER", src: challenger },
@@ -25,11 +26,15 @@ const Herocombo = () => {
       src: dart,
     },
   ];
+  const [activeSlide, setActiveSlide] = useState(0);
+  const handleChange = (index) => {
+    setActiveSlide(index);
+  };
   return (
     <section className="flex justify-between relative items-center h-[94.2%]">
       <div className="bg-[#595872] h-96 w-[45rem] blur-[140px] absolute left-80 bottom-44"></div>
-      <HeroText text={HeroVehicles} />
-      <HeroImage image={HeroVehicles} />
+      <HeroText text={HeroVehicles} active={activeSlide} />
+      <HeroImage image={HeroVehicles} handleSlideChange={handleChange} />
     </section>
   );
 };
