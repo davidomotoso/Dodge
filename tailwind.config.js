a9ee2d3f5d2c6b25f6b8f7c5eaf4df7b1eee59a3
davidomotoso <davidomotoso45@gmail.com>
2023-09-19 11:37:09 +0100

    Removed an unnecessary code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      screens: {
-        mobile: "300px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
